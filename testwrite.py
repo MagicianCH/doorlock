80469879ed06611ffb4245158558286cdaffc603
chenhao <chenhao_ustb@163.com>
2015-01-07 22:00:46 +0800

    2015-1-7

--- testwrite.py
+++ testwrite.py
@@ -1,16 +1,19 @@
 import MySQLdb
 import serial
+import time
+import string
 
-def dbWrite(_id):
+def dbWrite(_id,name):
     conn = MySQLdb.connect('localhost',user='root',passwd='chao',db='doorlock')
     cur = conn.cursor()
-    queryname = 'select name from memberinfo where id=' + "\'" + _id + "\'"
-    cur.execute(queryname)
-    name = "%s" % cur.fetchone()
-    print name
-    ll = "%s,%s" % (_id,name)
-    print ll
-    writestr = "insert into checktime values('%s','%s',now())" % (_id,name)
+
+#    queryname = 'select name from memberinfo where id=' + "\'" + _id + "\'"
+#    cur.execute(queryname)
+#    name = "%s" % cur.fetchone()
+#    print name
+#    ll = "%s,%s" % (_id,name)
+#   print ll
+    writestr = "insert into test values('%s','%s')" % (_id,name)
     print writestr
     cur.execute(writestr)
     cur.close()
@@ -18,5 +21,17 @@ def dbWrite(_id):
     conn.close()
 
 if __name__ == '__main__':
-    id = 'A2 EA E5 1F 00 BE 00'
-    dbWrite(id)
+    ser = serial.Serial('/dev/ttyUSB0','115200')
+    while True:
+        length = ser.read(1)
+        len = string.atoi(length)
+        data = ser.read(20+len)
+        print data
+        id = data[0:20]
+        name = data[21:(20+len)]
+        print name
+        dbWrite(id,name)
+
+        ser.flushInput()
+        time.sleep(1)
+
